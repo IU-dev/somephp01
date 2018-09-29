c5b7645191ea4bfd55e7e2f49447432c21977a03
student <student@obr18.ru>
2018-09-29 16:36:23 +0400

    Index.php Fix #2

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require_once 'includes/global.inc.php';
 $page = "index.php"
 ?>
 <html>
-<head>
-<title>Homepage</title>
-</head>
-<body>
-<?php require_once 'includes/header.inc.php'; ?>
-
-<?php $user = unserialize($_SESSION['user']); ?>
-<?php if(isset($_SESSION['logged_in'])) : ?>
-Hello, <?php echo $user->username; ?>. You are logged in. <a href="logout.php">Logout</a> 
-| <a href="settings.php">Change Email</a>
-<?php else : ?>
-You are not logged in, really. <a href="login.php">Log In</a> | <a href="register.php">Register</a>
-<?php endif; ?>
-</body>
+	<head>
+		<title>Главная | ШАРП</title>
+	</head>
+	<body>
+		<header><?php require_once 'includes/header.inc.php'; ?></header>
+		
+		<?php $user = unserialize($_SESSION['user']); ?>
+		
+		<?php if(isset($_SESSION['logged_in'])) : ?>
+			Hello, <?php echo $user->username; ?>. You are logged in. <a href="logout.php">Logout</a> | <a href="settings.php">Change Email</a>
+		<?php else : ?>
+			You are not logged in, really. <a href="login.php">Log In</a> | <a href="register.php">Register</a>
+		<?php endif; ?>
+		</body>
 </html>
