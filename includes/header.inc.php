a01538b75f31ee0c165110eb946b26e50e47e676
student <student@obr18.ru>
2018-09-29 16:22:13 +0400

    Create Bootstrap Header

---
+++ includes/header.inc.php
@@ -0,0 +1,30 @@
+<header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">ШАРП</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item <?php echo ($page == "index.php" ? "active" : "");?>">
+              <a class="nav-link" href="index.php">Главная</a>
+            </li>
+            <li class="nav-item <?php echo ($page == "index.php" ? "active" : "");?>">
+              <a class="nav-link" href="about.php">О проекте</a>
+            </li>
+            <li class="nav-item <?php echo ($page == "index.php" ? "active" : "");?>">
+              <a class="nav-link" href="introduction.php">Вступление</a>
+            </li>
+			<li class="nav-item <?php echo ($page == "index.php" ? "active" : "");?>">
+              <a class="nav-link" href="study.php">Обучение</a>
+            </li>
+			<li class="nav-item <?php echo ($page == "index.php" ? "active" : "");?>">
+              <a class="nav-link" href="competitions.php">Соревнования</a>
+            </li>
+          </ul>
+          <a class="btn btn-outline-success my-2 my-sm-0" type="button" href="login.php">Вход</a>
+		  <a class="btn btn-outline-info my-2 my-sm-0" type="button" href="register.php">Регистрация</a>
+          </form>
+        </div>
+      </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 //index.php 
 require_once 'includes/global.inc.php';
+$page = "index.php"
 ?>
 <html>
 <head>
 <title>Homepage</title>
 </head>
 <body>
-
+<?php require_once 'includes/header.inc.php'; ?>
 <?php if(isset($_SESSION['logged_in'])) : ?>
 <?php $user = unserialize($_SESSION['user']); ?>
 Hello, <?php echo $user->username; ?>. You are logged in. <a href="logout.php">Logout</a> 
